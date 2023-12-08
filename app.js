fe0eb0f480fbe4f380b9f8421394af1f3285c276
Ikhwana Fakih Nur Aziz <14151064+ikhwan-aziz@users.noreply.github.com>
2023-12-08 17:15:18 +0700

    Update DB connection on app.js

--- app.js
+++ app.js
@@ -23,10 +23,10 @@ app.set("view engine", "ejs");
 
 //Connecting to the MongoDB database.
 //useNew.. UseUnified.. were suggested by the server, don't know what they do.
-//mongoose.connect("mongodb+srv://fqbaik:Z3oktWntmd3fXmFn@praktikum.giynsvc.mongodb.net/?retryWrites=true&w=majority", {
-mongoose.connect("mongodb://localhost:27017", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+mongoose.connect("mongodb+srv://fqbaik:Z3oktWntmd3fXmFn@praktikum.giynsvc.mongodb.net/?retryWrites=true&w=majority", {
+//mongoose.connect("mongodb://localhost:27017", {
+ // useNewUrlParser: true,
+ // useUnifiedTopology: true,
 });
 
 var db = mongoose.connection;
