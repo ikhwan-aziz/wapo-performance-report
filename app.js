9d903aac86477d1d81b0df53380d6141beca8bf2
Ikhwana Fakih Nur Aziz <14151064+ikhwan-aziz@users.noreply.github.com>
2023-12-11 15:54:32 +0700

    Update app.js

--- app.js
+++ app.js
@@ -25,8 +25,8 @@ app.set("view engine", "ejs");
 //useNew.. UseUnified.. were suggested by the server, don't know what they do.
 mongoose.connect("mongodb+srv://silaki:wapo@silaki-wapo.markhfb.mongodb.net/?retryWrites=true&w=majority", {
 //mongoose.connect("mongodb://localhost:27017", {
- // useNewUrlParser: true,
- // useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
 var db = mongoose.connection;
